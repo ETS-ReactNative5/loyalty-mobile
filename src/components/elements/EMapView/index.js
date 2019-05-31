c9bef2f503510e170340bd1ec3a68b6416f53fd9
tienbui <tienbui94@gmail.com>
2019-05-31 16:52:06 +0700

    update marker for google map

--- src/components/elements/EMapView/index.js
+++ src/components/elements/EMapView/index.js
@@ -2,19 +2,32 @@ import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import React, { Component } from "react";
 import styles from './style'
 import PropTypes from 'prop-types'
-import { Marker } from 'react-native-maps';
+// import Marker  from 'react-native-maps';
 
 export default class EMapView extends Component {
-    // state = {
-    //     markers: [
-    //         {
-    //             latlng: {lat: 10.763337, lng: 106.671024 }
-    //         }
-    //     ]
-    // }
-  
-    render() {
+    constructor(props) {
+        super(props);
         
+          this.state = {
+            markers: [{
+                title: 'OKIE',
+                coordinates: {
+                  latitude: 10.804015,
+                  longitude: 106.675381
+                },
+              },
+              {
+                title: 'OKIE',
+                coordinates: {
+                  latitude: 10.803509,
+                  longitude: 106.678428
+                },  
+              }]
+         };
+        }
+
+    render() {
+       
       return (
                 <MapView 
                     provider={PROVIDER_GOOGLE}
@@ -27,17 +40,12 @@ export default class EMapView extends Component {
                     }}
                     mapType= 'standard'
                 >
-                {/* {this.state.markers.map((marker, index) => {
-                    const lat = marker.latlng.lat
-                    const lng = marker.latlng.lng
-                    console.log(lat, lng)
-                    return(
-                        <Marker
-                            key={index}
-                            coordinate={{lat, lng}}
-                        />
-                    )
-                })} */}
+                    {this.state.markers.map(marker => (
+                    <MapView.Marker 
+                    coordinate={marker.coordinates}
+                    title={marker.title}
+                    />
+                ))}
                 </MapView>
           );
       }
